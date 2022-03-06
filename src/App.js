849cf320e82f8e6d10c8593d668d79c569d3b244
Leo Bowen Wang <leobwang@gmail.com>
2022-03-06 17:01:24 +0100

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import Header from "./components/Header";
 import Chat from "./components/Chat";
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+;
 function App() {
   return (
     <><Header /><Chat /></>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,35 +2,34 @@ import { user, username } from "../user";
 
 function signout() {
   user.leave();
-  username.set('');
+  username.set("");
 }
 
 function Header() {
   return (
-    <header>
-      <h1>🔫💬</h1>
-      <section>
-        {username ? (
-            <div className="user-bio">
-              <span>
-                Hello <strong>{username}</strong>
-              </span>
-              <img
-                src={`https://avatars.dicebear.com/api/initials/${username}.svg`}
-                alt="avatar"
-              />
+    <div className="navbar navbar-expand-lg navbar-light bg-light">
+      <img src={`logo.png`} id="logo" className="img-fluid" alt="DeChat.gun" />
 
-            <button className="signout-button" onClick={signout}>
-              Sign Out
-            </button>
-            </div>
-        ):(
-          <div className="user-bio">
-            <h3>Gun.js Chat</h3>
-          </div>
-        )}
-      </section>
-    </header>
+      {username ? (
+        <div className="user-bio">
+          <span>
+            Hello <strong>{username}</strong>
+          </span>
+          <img
+            src={`https://avatars.dicebear.com/api/initials/${username}.svg`}
+            alt="avatar"
+          />
+
+          <button className="signout-button" onClick={signout}>
+            Sign Out
+          </button>
+        </div>
+      ) : (
+        <div className="user-bio">
+          <h3 className="text-center">DeChat.gun</h3>
+        </div>
+      )}
+    </div>
   );
 }
 export default Header;
