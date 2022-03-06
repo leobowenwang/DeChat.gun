bd23808b4fc83c2ffe1f40799181d647c718fa38
Leo Bowen Wang <leobwang@gmail.com>
2022-03-06 21:13:09 +0100

    WIP chat GUI

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,46 +1,50 @@
-import React, { useRef, useState, useEffect } from 'react';
-import GUN from 'gun';
-import { user, db, username } from '../user';
-import ChatMessage from './ChatMessage';
-import Login from './Login';
+import React, { useRef, useState, useEffect } from "react";
+import GUN from "gun";
+import { user, db, username } from "../user";
+import ChatMessage from "./ChatMessage";
+import Login from "./Login";
 // key for end-to-end encryption
 const key = require("../secrets.json");
-const sea = require('gun/sea')
+const sea = require("gun/sea");
 
 function Chat() {
   const [messages, setMessages] = useState([]);
-  const [newMessage, setNewMessage] = useState('');
+  const [newMessage, setNewMessage] = useState("");
 
   async function sendMessage() {
     const secret = await sea.encrypt(newMessage, key);
-    const message = user.get('all').set({ what: secret });
+    const message = user.get("all").set({ what: secret });
     const index = new Date().toISOString();
-    db.get('chat').get(index).put(message);
-    setNewMessage('');
+    db.get("chat").get(index).put(message);
+    setNewMessage("");
   }
 
   async function loadMessages() {
     var match = {
-      '.': {
-        '>': new Date(+new Date() - 1 * 1000 * 60 * 60 * 3).toISOString(),
+      ".": {
+        ">": new Date(+new Date() - 1 * 1000 * 60 * 60 * 3).toISOString(),
       },
-      '-': 1,
+      "-": 1,
     };
-    db.get('chat')
-        .map(match)
-        .once(async (data, id) => {
-          if (data) {
-            var message = {
-              // transform the data
-              who: await db.user(data).get('alias'), // a user might lie who they are! So let the user system detect whose data it is.
-              what: (await sea.decrypt(data.what, key)) + '', // force decrypt as text.
-              when: GUN.state.is(data, 'what'), // get the internal timestamp for the what property.
-            };
-            if (message.what) {
-              setMessages(prevState => [...prevState.slice(-100), message].sort((a, b) => a.when - b.when));
-            }
+    db.get("chat")
+      .map(match)
+      .once(async (data, id) => {
+        if (data) {
+          var message = {
+            // transform the data
+            who: await db.user(data).get("alias"), // a user might lie who they are! So let the user system detect whose data it is.
+            what: (await sea.decrypt(data.what, key)) + "", // force decrypt as text.
+            when: GUN.state.is(data, "what"), // get the internal timestamp for the what property.
+          };
+          if (message.what) {
+            setMessages((prevState) =>
+              [...prevState.slice(-100), message].sort(
+                (a, b) => a.when - b.when
+              )
+            );
           }
-        });
+        }
+      });
   }
 
   useEffect(() => {
@@ -48,20 +52,35 @@ function Chat() {
   }, []);
   if (user.is) {
     return (
-        <div className="App">
-          <section>
+      <div className="App">
+        <div class="padding">
+          <div class="row container d-flex justify-content-center">
+            <div class="col-md-6">
               <main>
-                {messages.length!=0 ? messages.map((message, key) => (
-                  <ChatMessage key={key} message={message} user={user} />
-                )) : null}
+                {messages.length != 0
+                  ? messages.map((message, key) => (
+                      <ChatMessage key={key} message={message} user={user} />
+                    ))
+                  : null}
               </main>
-              <input placeholder="Type a message..." onChange={e => setNewMessage(e.target.value)} maxLength="100" />
-              <button disabled={newMessage===''} onClick={sendMessage}>💥</button>
-          </section>
+            </div>
+            <div class="input-group">
+              <input
+                placeholder="Type a message..."
+                className="form-control"
+                onChange={(e) => setNewMessage(e.target.value)}
+                maxLength="100"
+              />
+              <button className="btn btn-primary" disabled={newMessage === ""} onClick={sendMessage}>
+                Send
+              </button>
+            </div>
+          </div>
         </div>
+      </div>
     );
   }
-  return (<Login/>)
+  return <Login />;
 }
 
-export default Chat
+export default Chat;
--- src/components/ChatMessage.jsx
+++ src/components/ChatMessage.jsx
@@ -1,22 +1,23 @@
-import { username } from '../user';
+import { username } from "../user";
 
 function ChatMessage(props) {
-    let message = props.message;
-    let sender = username;
-    const messageClass = message.who === sender ? 'sent' : 'received';
-    const avatar = `https://avatars.dicebear.com/api/initials/${message.who}.svg`;
-    const ts = new Date(message.when);
+  let message = props.message;
+  let sender = username;
+  const messageClass = message.who === sender ? "sent" : "received";
+  const avatar = `https://avatars.dicebear.com/api/initials/${message.who}.svg`;
+  const ts = new Date(message.when);
 
-    return (
-        <div className={`message ${messageClass}`}>
-            <img className='avatar' src={avatar} alt="avatar" />
-            <div className="message-text">
-                <p>{message.what}</p>
-                <time>{ts.toLocaleTimeString()}</time>
-            </div>
+  return (
+    <div className="card">
+      <div className={`message ${messageClass}`}>
+        <img className="avatar" src={avatar} alt="avatar" />
+        <div className="message-text">
+          <p>{message.what}</p>
+          <time>{ts.toLocaleTimeString()}</time>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default ChatMessage;
-
