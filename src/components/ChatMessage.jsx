f226081ff35319dc9c261a4bdfbbaa7fe138557e
Leo Bowen Wang <leobwang@gmail.com>
2022-03-11 13:00:28 +0100

    add figure

--- src/components/ChatMessage.jsx
+++ src/components/ChatMessage.jsx
@@ -3,20 +3,17 @@ function ChatMessage(props) {
   let sender = props.username;
   const messageClass = message.who === sender ? "sent" : "received";
   const avatar = `https://avatars.dicebear.com/api/initials/${message.who}.svg`;
-  const ts = new Date(message.when);
+  const ts = new Date(message.when).toLocaleTimeString();
 
   return (
     <div className={`message ${messageClass}`}>
-      <img
-        className={`avatar avatar-${messageClass}`}
-        src={avatar}
-        alt="avatar"
-      />
+      <figure className={`figure figure-${messageClass}`}>
+        <img className="avatar" src={avatar} alt="avatar" />
+        <figcaption class="figure-caption">{message.who}</figcaption>
+      </figure>
       <div className={`message-text-${messageClass}`}>
         <p>{message.what}</p>
-        <time className={`time-${messageClass}`}>
-          {ts.toLocaleTimeString()}
-        </time>
+        <time className={`time-${messageClass}`}>{ts}</time>
       </div>
     </div>
   );
