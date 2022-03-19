ffdb98116519b8f3870040c1b4cc35e1dc659dd5
Leo Bowen Wang <leobwang@gmail.com>
2022-03-19 15:11:20 +0100

    aws port

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const app = express();
 const port = 3030;
 app.use(GUN.serve);
 
-const server = app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+const server = app.listen(process.env.port || port, () => {
+  console.log(`Example app listening at http://${process.env.PUBLIC_URL}:${port}`);
 });
 
 GUN({ web: server });
